9a3c682d567220d1c5ce8dca422d74ffac68ed57
tanshiqi <tanshiqi@gmail.com>
2024-10-12 11:40:59 +0800

    wip

--- components/dashboard/Breadcrumb.vue
+++ components/dashboard/Breadcrumb.vue
@@ -14,7 +14,7 @@ defineProps({
     <BreadcrumbList>
       <BreadcrumbItem>
         <BreadcrumbLink href="/">
-          Sink
+          ddz.to
         </BreadcrumbLink>
       </BreadcrumbItem>
       <BreadcrumbSeparator />
