58a1c8e06989c6a705b3d5c1c4387b2edc29b583
tanshiqi <tanshiqi@gmail.com>
2024-10-12 11:21:33 +0800

    test

--- components/layouts/Footer.vue
+++ components/layouts/Footer.vue
@@ -23,6 +23,79 @@ onMounted(() => {
       >
         &copy; {{ new Date().getFullYear() }} Based on the open-source project from Sink
       </a>
+      <span
+        class="inline-flex justify-center mt-4 space-x-5 sm:ml-auto sm:mt-0 sm:justify-start"
+      >
+        <a
+          ref="email"
+          href="mailto:sink.cool$miantiao.me"
+          title="Email"
+          class="text-gray-400 hover:text-gray-500"
+        >
+          <span class="sr-only">Email</span>
+          <GmailIcon
+            class="w-6 h-6"
+          />
+        </a>
+        <a
+          href="https://t.me/htmlzone"
+          target="_blank"
+          title="Telegram"
+          class="text-gray-400 hover:text-gray-500"
+        >
+          <span class="sr-only">Telegram</span>
+          <TelegramIcon
+            class="w-6 h-6"
+          />
+        </a>
+        <a
+          href="https://mt.ci"
+          target="_blank"
+          title="Blog"
+          class="text-gray-400 hover:text-gray-500"
+        >
+          <span class="sr-only">Blog</span>
+          <BloggerIcon
+            class="w-6 h-6"
+          />
+        </a>
+
+        <a
+          href="https://x.com/0xKaiBi"
+          target="_blank"
+          title="Twitter"
+          class="text-gray-400 hover:text-gray-500"
+        >
+          <span class="sr-only">Twitter</span>
+          <XIcon
+            class="w-6 h-6"
+          />
+        </a>
+
+        <a
+          href="https://miantiao.me/@chi"
+          target="_blank"
+          title="Mastodon"
+          class="text-gray-400 hover:text-gray-500"
+        >
+          <span class="sr-only">Mastodon</span>
+          <MastodonIcon
+            class="w-6 h-6"
+          />
+        </a>
+
+        <a
+          href="https://github.com/ccbikai"
+          target="_blank"
+          title="GitHub"
+          class="text-gray-400 hover:text-gray-500"
+        >
+          <span class="sr-only">GitHub</span>
+          <GitHubIcon
+            class="w-6 h-6"
+          />
+        </a>
+      </span>
     </div>
   </section>
 </template>
